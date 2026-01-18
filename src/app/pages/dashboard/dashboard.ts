afe984c07768ff9939443a2537bfcab2bcb44be9
pedrozc90 <pedrozc90@gmail.com>
2026-01-18 18:58:43 -0300

    change dashboard to 'effect' pattern

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, computed, effect, inject, OnInit, signal } from "@angular/core";
 import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 
 import { Document, DocumentType } from "../../core/types";
 import { DocumentStatusPipe, DocumentTypePipe } from "../../core/pipes";
 import { FileService } from "../../core/services";
+import { debounceTime } from "rxjs";
+import { toSignal } from "@angular/core/rxjs-interop";
 
 interface Filters {
     q: string;
@@ -32,7 +34,7 @@ type FiltersFormGroup = FormGroup & {
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class Dashboard implements OnInit {
+export class Dashboard {
 
     private fileService = inject(FileService);
 
@@ -40,6 +42,11 @@ export class Dashboard implements OnInit {
     readonly form = new FormGroup({
         q: new FormControl<string>("", [Validators.minLength(1)])
     }) as FiltersFormGroup;
+
+    private filters = toSignal(
+            this.form.valueChanges.pipe(debounceTime(300)),
+            { initialValue: this.form.value }
+        );
 
     // --- state ---
     public page = signal(1);
@@ -51,47 +58,45 @@ export class Dashboard implements OnInit {
     public hasPrev = computed(() => this.page() > 1);
     public hasNext = computed(() => this.page() * this.rows < this.total());
 
-    public ngOnInit(): void {
-        this.load();
-    }
+    constructor() {
+        effect((onCleanup) => {
+            if (!this.loading()) {
+                this.loading.set(true);
+            }
 
-    public load(): void {
-        this.loading.set(true);
+            const value = this.filters();
+            const params = {
+                page: this.page(),
+                q: value.q || undefined
+            }
 
-        const value = this.form.value;
-        const params = {
-            page: this.page(),
-            q: value.q || undefined
-        }
+            const sub = this.fileService.fetch(params).subscribe({
+                next: (res) => {
+                    const t = typeof res.list[0].type;
+                    const s = typeof res.list[0].status;
+                    this.total.set(res.total);
+                    this.files.set(res.list);
+                },
+                error: () => this.files.set([]),
+                complete: () => this.loading.set(false)
+            });
 
-        this.fileService.fetch(params).subscribe({
-            next: (res) => {
-                const t = typeof res.list[0].type;
-                const s = typeof res.list[0].status;
-                console.log(t, res.list[0].type === DocumentType.INPUT, s);
-                this.total.set(res.total);
-                this.files.set(res.list);
-            },
-            error: () => this.files.set([]),
-            complete: () => this.loading.set(false)
+            onCleanup(() => sub.unsubscribe());
         });
     }
 
     public next(): void {
         if (!this.hasNext()) return;
         this.page.update((p) => p + 1);
-        this.load();
     }
 
     public prev(): void {
         if (!this.hasPrev()) return;
         this.page.update((p) => p - 1);
-        this.load();
     }
 
     public search(): void {
         this.page.set(1);
-        this.load();
     }
 
 }
